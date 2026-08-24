3fe4fb4708d9f436b392711d96dea7a81eed11fb
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-24 23:59:24 +0530

    feat: add signup and signin endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,21 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import mongoose from "mongoose"
+import authRoutes from "./routes/authRoute.js"
+dotenv.config();
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/auth", authRoutes);
+
+app.get("/" , (req , res)=> {
+    res.json({message: "Teamflow API running"})
+})
+
+//connecting db
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
@@ -14,21 +28,10 @@ const connectDB = async () => {
     }
 }
 
-dotenv.config();
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/" , (req , res)=> {
-    res.json({message: "Teamflow API running"})
-})
-
 const PORT = process.env.PORT
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 });
