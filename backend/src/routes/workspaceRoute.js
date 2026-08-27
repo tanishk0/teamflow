57f78ef33fed24dec59c7f863883edc6b8ee6781
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-27 15:26:52 +0530

    feat: add patch route for renameWorkspace

--- backend/src/routes/workspaceRoute.js
+++ backend/src/routes/workspaceRoute.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { createWorkspace, getWorkspaces } from "../controllers/workspaceController.js"
+import { createWorkspace, getWorkspaces, renameWorkspace } from "../controllers/workspaceController.js"
 import { requireAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", requireAuth, createWorkspace);
 router.get("/", requireAuth, getWorkspaces);
-
+router.patch("/:id", requireAuth, renameWorkspace);
 export default router;
