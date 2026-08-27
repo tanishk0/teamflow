c2c50c23e26abc2092accd4c72a8db1a6104b547
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-27 15:03:05 +0530

    createworkspace

---
+++ backend/src/controllers/workspaceController.js
@@ -0,0 +1,30 @@
+import Workspace from "../db/Workspace.js";
+import WorkspaceMember from "../db/WorkspaceMember.js";
+
+export async function createWorkspace(req , res){
+    const { name } = req.body;
+    try {
+        const workspace = await Workspace.create({
+            name: name,
+            owner: req.userId
+        })
+        //creating the owner the first member
+        await WorkspaceMember.create({
+            workspaceId: workspace._id,
+            userId: req.userId,
+            role: "owner",
+            status: "active"
+        })
+        res.status(201).json({
+            message: "Workspace created successfully",
+            workspace
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            message: "Failed to create workspace",
+            error: error.message
+        })
+    }
+}
