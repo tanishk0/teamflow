f9c7d52e7363c92d931d08d093454ebd7fea9584
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-29 22:53:48 +0530

    feat: create new workspace button added

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar.jsx";
 import api from "../src/api/axios.js";
 import Button from "../components/Button.jsx";
+import WorkspaceModal from "../components/modals/AddWorkspaceModal.jsx";
 export default function Dashboard() {
   const [workspaces, setWorkspaces] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   const items = [
     { label: "Overview", path: "/dashboard" },
@@ -11,6 +13,7 @@ export default function Dashboard() {
     { label: "Invitations", path: "/invitations" },
     { label: "Teams", path: "/teams" },
   ];
+
   useEffect(() => {
     async function fetchWorkspaces() {
       try {
@@ -24,14 +27,36 @@ export default function Dashboard() {
 
     fetchWorkspaces();
   }, []);
+
+  //Create workspace through add workspace button
+  async function handleCreateWorkspace(name) {
+    try {
+      const response = await api.post("/workspaces", {
+        name,
+      });
+      const workspace = response.data.workspace;
+      //Add newly added workspace to ui
+      setWorkspaces((prevWorkspaces) => [...prevWorkspaces, workspace]);
+      // Close the modal
+      setShowModal(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="flex min-h-screen">
       <Sidebar items={items} />
 
-      <div className="p-4 flex flex-col">
-        <h2 className="text-3xl font-semibold">Dashboard</h2>
+      <div className="p-4 flex flex-col w-full">
+        <div className="w-full p-2 flex justify-between">
+          <h2 className="text-3xl font-semibold">Dashboard</h2>
+          <Button
+            text="Add workspace"
+            onClick={() => setShowModal(true)}
+          ></Button>
+        </div>
 
-        <div>
+        <div className="flex flex-col gap-1 mt-4">
           {workspaces.length === 0 ? (
             <Button text="Add workspace" />
           ) : (
@@ -43,13 +68,20 @@ export default function Dashboard() {
                 <h3 className="text-lg font-semibold">
                   {workspace.workspaceId.name}
                 </h3>
-
-                <p className="text-sm text-gray-500 mt-1">Your workspace</p>
               </div>
             ))
           )}
         </div>
       </div>
+      <div className="w-[32%] flex flex-col p-2 bg-white">
+        <h1>Activity Log</h1>
+      </div>
+      {showModal && (
+        <WorkspaceModal
+          onClose={() => setShowModal(false)}
+          onCreate={handleCreateWorkspace}
+        />
+      )}
     </div>
   );
 }
