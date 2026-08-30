ba243c76dede4ada1189f5c00d2390cd1ce95e01
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-30 18:05:59 +0530

    create invitation controller file

---
+++ backend/src/controllers/invitationController.js
@@ -0,0 +1,6 @@
+import Workspace from "../db/Workspace";
+import WorkspaceMember from "../db/WorkspaceMember";
+
+export async function createInvite(){
+    
+}
--- frontend/pages/WorkspaceDetail.jsx
+++ frontend/pages/WorkspaceDetail.jsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
 export default function WorkspaceDetail(){
-    
+    const { id } = useParams();
+    const items = [
+    { label: "Projects", path: "/" },
+    { label: "Activity Log", path: "/" },
+    { label: "Members", path: "/" },
+    { label: "Settings", path: "/" },
+  ];
+    return(
+        <>
+        <div className="flex min-h-screen">
+            <Sidebar items={items}></Sidebar>
+            
+        </div>
+        </>
+    )
 }
