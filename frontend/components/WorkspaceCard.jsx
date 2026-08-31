b071d38da77b5fa5e06aaa8f51f7d0d746c890ce
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-31 23:10:30 +0530

    feat: added rename state

--- frontend/components/WorkspaceCard.jsx
+++ frontend/components/WorkspaceCard.jsx
@@ -1,10 +1,27 @@
 import { MoreVertical } from "lucide-react";
 import { DropdownMenu } from "radix-ui";
+import { useState } from "react";
 
 export default function WorkspaceCard({ workspace, onRename, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(workspace.name);
   return (
     <div className="w-full h-16 text-md flex justify-between items-center px-8 bg-white rounded-md shadow-sm my-1">
-      <p>{workspace.name}</p>
+      {isEditing ? (
+        <input
+          autoFocus
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              onRename(workspace._id, name);
+              setIsEditing(false);
+            }
+          }}
+        />
+      ) : (
+        <p>{workspace.name}</p>
+      )}
 
       <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild>
@@ -20,7 +37,10 @@ export default function WorkspaceCard({ workspace, onRename, onDelete }) {
             className="w-32 bg-white rounded-lg shadow-lg overflow-hidden z-50 p-1"
           >
             <DropdownMenu.Item
-              onSelect={() => onRename(workspace)}
+              onSelect={() => {
+                setName(workspace.name);
+                setIsEditing(true);
+              }}
               className="px-4 py-2 cursor-pointer outline-none hover:bg-gray-100 rounded-md"
             >
               Rename
