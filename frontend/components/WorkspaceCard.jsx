69b830ebcd2b939a76fec6c58c1db60bdcc7e197
tanishk0 <tanishkaggarwal245@gmail.com>
2026-08-31 23:02:44 +0530

    refactor: styling changes

--- frontend/components/WorkspaceCard.jsx
+++ frontend/components/WorkspaceCard.jsx
@@ -3,7 +3,7 @@ import { DropdownMenu } from "radix-ui";
 
 export default function WorkspaceCard({ workspace, onRename, onDelete }) {
   return (
-    <div className="w-full h-16 flex justify-between items-center px-8 bg-white rounded-md shadow-sm my-1">
+    <div className="w-full h-16 text-md flex justify-between items-center px-8 bg-white rounded-md shadow-sm my-1">
       <p>{workspace.name}</p>
 
       <DropdownMenu.Root>
@@ -17,18 +17,18 @@ export default function WorkspaceCard({ workspace, onRename, onDelete }) {
           <DropdownMenu.Content
             sideOffset={8}
             align="end"
-            className="w-32 bg-white rounded-lg shadow-lg overflow-hidden z-50"
+            className="w-32 bg-white rounded-lg shadow-lg overflow-hidden z-50 p-1"
           >
             <DropdownMenu.Item
               onSelect={() => onRename(workspace)}
-              className="px-4 py-2 cursor-pointer outline-none hover:bg-gray-100"
+              className="px-4 py-2 cursor-pointer outline-none hover:bg-gray-100 rounded-md"
             >
               Rename
             </DropdownMenu.Item>
 
             <DropdownMenu.Item
               onSelect={() => onDelete(workspace._id)}
-              className="px-4 py-2 cursor-pointer outline-none text-red-600 hover:bg-red-50"
+              className="px-4 py-2 cursor-pointer outline-none text-red-600 hover:bg-red-50 rounded-md"
             >
               Delete
             </DropdownMenu.Item>
