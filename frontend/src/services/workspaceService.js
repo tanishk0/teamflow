997c65f238f1386f615c87816d352189970cd83b
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-03 22:51:40 +0530

    createWorkspace api call

--- frontend/src/services/workspaceService.js
+++ frontend/src/services/workspaceService.js
@@ -4,3 +4,8 @@ export async function getWorkspace(){
     const response = await api.get('/workspace');
     return response.data.workspaces;
 }
+
+export async function createWorkspace(name){
+    const response = await api.post('/workspaces', {name})
+    return response.data.workspace;
+}
