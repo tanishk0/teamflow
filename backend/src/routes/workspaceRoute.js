07a6b018abe1fc71e1f064584663394f8a22b8d4
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-03 23:00:51 +0530

    add  workspace invitation routes

--- backend/src/routes/workspaceRoute.js
+++ backend/src/routes/workspaceRoute.js
@@ -7,5 +7,9 @@ const router = express.Router();
 router.post("/", requireAuth, createWorkspace);
 router.get("/", requireAuth, getWorkspaces);
 router.patch("/:id", requireAuth, renameWorkspace);
-router.delete("/:id", requireAuth, deleteWorkspace)
+router.delete("/:id", requireAuth, deleteWorkspace);
+
+//Workspace Invitation routes
+router.post("/:workspaceId/invitations", requireAuth, createInvite);
+router.get("/:workspaceId/invitations", requireAuth, getWorkspaceInvites);
 export default router;
