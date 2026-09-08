7598827ee7c5cd0c42d4ec130c4e84b3e92299f1
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-08 23:51:35 +0530

    add /auth/me get route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { logout, signin , signup } from "../controllers/authController.js";
+import { logout, signin , signup, getMe } from "../controllers/authController.js";
+import { requireAuth } from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
 router.post("/register" , signup);
 router.post("/login" , signin);
+router.get("/me" , requireAuth, getMe);
 router.post("/logout", logout);
-
 export default router;
