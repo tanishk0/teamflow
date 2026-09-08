468da8006a2f2c00fa2c94cfc7bf204b17a34519
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-08 23:51:08 +0530

    add getMe function

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -108,3 +108,25 @@ export async function logout(req, res){
     res.clearCookie("token");
     res.json({ message: "Logged out successfully" });
 }
+
+//auth/me
+
+export async function getMe(req, res) {
+    try {
+        const user = await User.findById(req.userId).select("name email");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+        return res.status(200).json({
+            user,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to fetch user",
+        });
+    }
+}
