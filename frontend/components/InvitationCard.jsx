765bc4f2d7c326e97d8882f9c3cd37e54f342ebd
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-12 23:23:51 +0530

    add styling

--- frontend/components/InvitationCard.jsx
+++ frontend/components/InvitationCard.jsx
@@ -2,7 +2,7 @@ export default function InvitationCard({ invite, type, onAccept, onReject }) {
   const isTeamInvite = type === "team";
 
   return (
-    <div className="bg-white rounded-md p-5 flex items-center justify-between">
+    <div className="bg-white rounded-md p-5 w-full flex items-center justify-between">
       <div>
         <h3 className="text-xl font-medium">
           {isTeamInvite ? invite.teamId.name : invite.workspaceId.name}
@@ -16,9 +16,9 @@ export default function InvitationCard({ invite, type, onAccept, onReject }) {
       </div>
 
       <div className="flex gap-3">
-        <button onClick={() => onReject(invite._id)}>Reject</button>
+        <button className="text-danger cursor-pointer" onClick={() => onReject(invite._id)}>Reject</button>
 
-        <button onClick={() => onAccept(invite._id)}>Accept</button>
+        <button className="text-white bg-primary p-2 rounded-md cursor-pointer" onClick={() => onAccept(invite._id)}>Accept</button>
       </div>
     </div>
   );
