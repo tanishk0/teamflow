90189eb02a60c0d398a9143b68a8b659975f94a1
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-22 22:58:11 +0530

    add task schema

---
+++ backend/src/db/Task.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    status: {
+        type: String,
+        enum: ["todo", "in_progress", "done"],
+        default: "todo",
+    },
+
+    assigneeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    priority: {
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "low",
+    },
+
+    dueDate: {
+        type: Date,
+        default: null
+    },
+
+    section: {
+        type: String,
+        trim: true,
+    },
+}, {timestamps: true}
+)
