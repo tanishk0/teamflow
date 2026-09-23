f1e707b1c52788ca6d114119660337c6a724d30b
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-23 22:23:01 +0530

    add isOwner middleware to team and workspace delete and rename

--- backend/src/routes/teamRoute.js
+++ backend/src/routes/teamRoute.js
@@ -1,14 +1,16 @@
 import express from "express"
 import { createTeam, renameTeam, deleteTeam, getTeams, removeMember, getTeam } from "../controllers/teamController.js"
 import { requireAuth } from "../middleware/authMiddleware.js"
+import { isOwner } from "../middleware/isOwner.js";
+import Team from "../db/Team.js";
 
 const router = express.Router();
 
 router.post('/', requireAuth, createTeam);
 router.get('/', requireAuth, getTeams);
-router.patch('/:id', requireAuth, renameTeam);
-router.delete('/:id', requireAuth, deleteTeam);
-router.delete('/:id/members', requireAuth, removeMember);
+router.patch('/:id', requireAuth, isOwner(Team, "ownerId"), renameTeam);
+router.delete('/:id', requireAuth, isOwner(Team, "ownerId"),deleteTeam);
+router.delete('/:id/members', requireAuth, isOwner(Team, "ownerId"), removeMember);
 router.get("/:id", requireAuth, getTeam);
 
 export default router;
--- backend/src/routes/workspaceRoute.js
+++ backend/src/routes/workspaceRoute.js
@@ -5,13 +5,15 @@ import {
   createInvite,
   getWorkspaceInvites,
 } from "../controllers/invitationController.js";
+import { isOwner } from "../middleware/isOwner.js";
+import Workspace from "../db/Workspace.js";
 
 const router = express.Router();
 
 router.post("/", requireAuth, createWorkspace);
 router.get("/", requireAuth, getWorkspaces);
-router.patch("/:id", requireAuth, renameWorkspace);
-router.delete("/:id", requireAuth, deleteWorkspace);
+router.patch("/:id", requireAuth, isOwner(Workspace, "owner"), renameWorkspace);
+router.delete("/:id", requireAuth, isOwner(Workspace, "owner"), deleteWorkspace);
 
 //Workspace Invitation routes
 router.post("/:workspaceId/invitations", requireAuth, createInvite);
