c42c2be162fad2f0ed2d278e03a71c13caea6a1b
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-25 23:15:19 +0530

    create project routes

---
+++ backend/src/routes/projectRoute.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+import {
+    createProject, getProjects, getProject, renameProject, deleteProject
+} from "../controllers/projectController.js"
+import { requireAuth } from "../middleware/authMiddleware.js"
+
+const router = express.Router();
+
+router.post("/:workspaceId/projects", requireAuth, createProject);
+router.get("/:workspaceId/projects", requireAuth, getProjects);
+router.get("/:workspaceId/projects/:projectId", requireAuth, getProject);
+router.patch("/:workspaceId/projects/:projectId", requireAuth, renameProject);
+router.delete("/:workspaceId/projects/:projectId", requireAuth, deleteProject);
+
+export default router;
+
